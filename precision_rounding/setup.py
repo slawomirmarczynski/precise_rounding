faf2c7e953f84062a5fae3aa0666543e16f3265e
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2024-11-02 11:25:38 +0100

    Initial commit (version 0.1)
    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- precision_rounding/setup.py
+++ precision_rounding/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 
+
 setup(
     name='precision_rounding',
     version='0.1',
@@ -9,5 +10,6 @@ setup(
     license='BSD-3-Clause',
     author='Sławomir Marczyński',
     author_email='',
-    description='Rounds a measurement value and its uncertainty to a specified number of significant digits.'
+    description='Rounds a measurement value and its uncertainty '
+                'to a specified number of significant digits.'
 )
