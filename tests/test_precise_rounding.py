b107758e5e29dfb883c52b3975656ff8594c8a92
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2024-12-19 22:12:02 +0100

    Unit tests
    Signed-off-by: Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>

--- tests/test_precise_rounding.py
+++ tests/test_precise_rounding.py
@@ -12,35 +12,38 @@ from precise_rounding import precise_rounding
 class Test(TestCase):
 
     def test_01(self):
-        """oczywiste użycie, domyślna ilość cyfr"""
+        """simple and obvious, default precision"""
         supplied = 123.456789, 0.01269
         expected = '123.457', '0.013'
         result = precise_rounding(*supplied)
         self.assertEqual(expected, result)
 
     def test_02(self):
-        """oczywiste użycie, trzy parametry"""
+        """simple and obvious, third parameter"""
         supplied = 123.456789, 0.01269, 3
         expected = '123.4568', '0.0127'
         result = precise_rounding(*supplied)
         self.assertEqual(expected, result)
 
     def test_03(self):
-        """zerowa niepewność, dwa parametry"""
-        supplied = 123.456789, 0
-        expected = '123.456789', '0'
-        result = precise_rounding(*supplied)
-        self.assertEqual(expected, result)
+        """zero uncertainty, default precision"""
+        cases = (((123.456789, 0), ('123.456789', '0')),
+                 ((123.000000, 0), ('123', '0')),
+                 ((12300.0000, 0), ('12300', '0')),)
+        for supplied, expected in cases:
+            with self.subTest(supplied=supplied):
+                result = precise_rounding(*supplied)
+                self.assertEqual(expected, result)
 
     def test_04(self):
-        """zerowa niepewność, trzy parametry"""
+        """zero uncertainty, custom precision"""
         supplied = 123.456789, 0, 4
         expected = '123.456789', '0'
         result = precise_rounding(*supplied)
         self.assertEqual(expected, result)
 
     def test_05(self):
-        """zaokrąglanie wartości"""
+        """values' rounding"""
         cases = (((123.000, 0.1), ('123.00', '0.10')),
                  ((123.001, 0.1), ('123.00', '0.10')),
                  ((123.002, 0.1), ('123.00', '0.10')),
@@ -67,7 +70,7 @@ class Test(TestCase):
                 self.assertEqual(expected, result)
 
     def test_06(self):
-        """zaokrąglanie wartości, jedna cyfra znacząca niepewności"""
+        """values' rounding, one digit"""
         cases = (((123.000, 0.01, 1), ('123.00', '0.01')),
                  ((123.001, 0.01, 1), ('123.00', '0.01')),
                  ((123.002, 0.01, 1), ('123.00', '0.01')),
@@ -94,7 +97,7 @@ class Test(TestCase):
                 self.assertEqual(expected, result)
 
     def test_07(self):
-        """zaokrąglanie wartości, cztery cyfry znaczące niepewności"""
+        """values' rounding, four digits"""
         cases = (((123.000000, 0.01, 4), ('123.00000', '0.01000')),
                  ((123.000001, 0.01, 4), ('123.00000', '0.01000')),
                  ((123.000002, 0.01, 4), ('123.00000', '0.01000')),
@@ -121,7 +124,7 @@ class Test(TestCase):
                 self.assertEqual(expected, result)
 
     def test_08(self):
-        """zaokrąglanie niepewności w górę"""
+        """uncertainty rounded up"""
         cases = (
             ((123.456789, 0.01234567, 1), ('123.46', '0.02')),
             ((123.456789, 0.01234567, 2), ('123.457', '0.013')),
@@ -140,7 +143,7 @@ class Test(TestCase):
                 self.assertEqual(expected, result)
 
     def test_09(self):
-        """zaokrąglanie niepewności w dół"""
+        """uncertainty rounded down"""
         cases = (
             ((123.456789, 0.010234567, 1), ('123.46', '0.01')),
             ((123.456789, 0.012034567, 2), ('123.457', '0.012')),
@@ -156,8 +159,26 @@ class Test(TestCase):
                 result = precise_rounding(*supplied)
                 self.assertEqual(expected, result)
 
+    def test_10(self):
+        """promotion, uncertainty rounded up"""
+        cases = (
+            ((123.456789, 0.0999999, 1), ('123.5', '0.1')),
+            ((123.456789, 0.0999999, 2), ('123.5', '0.10')),
+            ((123.456789, 0.0999999, 3), ('123.457', '0.100')),
+            ((123.456789, 0.0999999, 4), ('123.4568', '0.1000')),
+            ((123.456789, 0.0999999, 5), ('123.45679', '0.10000')),
+            ((123.456789, 0.0999999, 6), ('123.456789', '0.100000')),
+            ((123.456789, 0.0999999, 7), ('123.4567890', '0.1000000')),
+            ((123.456789, 0.0999999, 8), ('123.45678900', '0.10000000')),
+            ((123.456789, 0.0999999, 9), ('123.456789000', '0.100000000')),)
+
+        for supplied, expected in cases:
+            with self.subTest(supplied=supplied):
+                result = precise_rounding(*supplied)
+                self.assertEqual(expected, result)
+
     def test_exceptions(self):
-        """testowanie wyjątków"""
+        """exception handling"""
 
         cases = ((('abc', 0.01), TypeError), ((10.0, 'abc'), TypeError),
                  ((1.0, 0.01, 'abc'), TypeError), ((1.0, -0.01), ValueError),
